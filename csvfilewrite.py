d16aa60d7383a38bd8b4f9eb698d9daf207fe3f3
SLkishore <45119357+SLkishore@users.noreply.github.com>
2022-06-21 23:21:21 +0530

    Update csvfilewrite.py

--- csvfilewrite.py
+++ csvfilewrite.py
@@ -1,7 +1,7 @@
 import csv  
 
 header = ['name', 'area', 'country_code2', 'country_code3']
-data = ['Afghanistan', 652090, 'AF', 'AFG']
+data = ['india', 652090, 'AF', 'AFG']
 
 with open('countries.csv', 'w', encoding='UTF8') as f:
     writer = csv.writer(f)
